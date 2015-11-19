6b1c3e45c7e7e3a2608f2b23e5161e1a15b66793
Ayer Chan <ochan4@fordham.edu>
2015-11-19 17:40:28 -0500

    11.19 OneDirection Project New

---
+++ Tile.swift
@@ -0,0 +1,27 @@
+//
+//  Tile.swift
+//  Sudoku2048
+//
+//  Created by Oi I Chan on 11/17/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class Tile: CCNode {
+    weak var valueLabel: CCLabelTTF!
+    weak var backgroundNode: CCNodeColor!
+    var mergedThisRound = false
+    
+    
+    
+    var value: Int = 0 {
+        //observer: updates the text of the label with the current value of the tile.
+        didSet {
+            if valueLabel != nil{
+                valueLabel.string = "\(value)"
+            }
+        }
+    }
+    
+}
